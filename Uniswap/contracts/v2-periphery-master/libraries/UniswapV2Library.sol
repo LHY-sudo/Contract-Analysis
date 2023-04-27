75ec63e8817e414fc5a46ff9a140a6372c45310f
LHY-sudo <archilhy@gmail.com>
2023-04-27 17:04:52 +0800

    Uniswap

--- Uniswap/contracts/v2-periphery-master/libraries/UniswapV2Library.sol
+++ Uniswap/contracts/v2-periphery-master/libraries/UniswapV2Library.sol
@@ -5,9 +5,11 @@ import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 import "./SafeMath.sol";
 
 library UniswapV2Library {
+    //使用safeMath库；
     using SafeMath for uint;
 
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    //对Token进行排序
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
@@ -15,8 +17,11 @@ library UniswapV2Library {
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
+    //计算Pair地址；
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        //地址排序
         (address token0, address token1) = sortTokens(tokenA, tokenB);
+        //计算create2部署和约的地址，
         pair = address(uint(keccak256(abi.encodePacked(
                 hex'ff',
                 factory,
@@ -26,20 +31,27 @@ library UniswapV2Library {
     }
 
     // fetches and sorts the reserves for a pair
+    //获取Token余额
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
+        //地址排序
         (address token0,) = sortTokens(tokenA, tokenB);
+        //调用Pair的getReaerve()函数；
         (uint reserve0, uint reserve1,) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
+        //合约地址
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
     // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+    //计算交换应得Token数量
     function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
+        //计算应该收到多啊少TokenB
         require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
         require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         amountB = amountA.mul(reserveB) / reserveA;
     }
 
     // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    //
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
         require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
